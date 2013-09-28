f0c3b2865dd354f66be5667710c2b84f2f2fd9d4
unknown <dosen.it2011@gmail.com>
2013-09-28 14:21:36 +0700

    commits02

---
+++ BiodataKu2.java
@@ -0,0 +1,12 @@
+//Perintah penggunaan package versi 01
+package Latihan01.sesi1.bin;
+
+public class BiodataKu2 
+{
+	public static void main(String[] data) {
+		System.out.println("N a m a        : Sri Rezeki Candra Nursari");
+		System.out.println("Alamat         : Depok");
+		System.out.println("Golongan Darah : B");
+		System.out.println("Jenis Kelamain : Perempuan");
+	}
+}
