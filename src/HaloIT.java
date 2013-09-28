2cd11cde9aef224e7eca8b593a3c13152a09fb55
unknown <dosen.it2011@gmail.com>
2013-09-28 14:34:55 +0700

    commits05

---
+++ src/HaloIT.java
@@ -0,0 +1,8 @@
+//Perintah pertama - contoh 01
+package Latihan01.sesi1.bin;
+
+public class HaloIT {
+	public static void main(String[] xx) {
+		System.out.println("Halo Teknik Informatika");
+	}
+}
